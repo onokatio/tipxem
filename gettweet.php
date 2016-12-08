2edc6d49d9dd79c161e3b4f0adaeb575da168ccd
onokatio <onokatio@gmail.com>
2016-12-08 16:23:20 +0900

    first tweet

--- gettweet.php
+++ gettweet.php
@@ -1,9 +1,11 @@
 <?php
-require 'vendor/autoload.php';
-require 'key.php';
-use mpyw\Co\Co;
-use mpyw\Co\CURLException;
-use mpyw\Cowitter\Client;
-use mpyw\Cowitter\HttpException;
+	require 'vendor/autoload.php';
+	require 'key.php';
+	use mpyw\Co\Co;
+	use mpyw\Co\CURLException;
+	use mpyw\Cowitter\Client;
+	use mpyw\Cowitter\HttpException;
 
+	$client = new Client([CK, CS, AT, ATS]);
+	$client->post('statuses/update', ['status' => 'test tweet by api']);
 ?>
