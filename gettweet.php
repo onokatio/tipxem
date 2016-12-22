01cb5af837c727c36d984d17fa541621982f6ecd
onokatio <onokatio@gmail.com>
2016-12-22 21:37:43 +0900

    testing co::wait

--- gettweet.php
+++ gettweet.php
@@ -1,20 +1,15 @@
 <?php
 	require __DIR__ . '/vendor/autoload.php'; //composerの自動ロード
-	require 'key.php'; //TwitterAPIのdefine読み込み
+	require 'key.php'; //TwitterAPIのdefine読み込み todo パス設定
 
-	use mpyw\Co\Co; //名前空間を干渉しないようにするための設定
-//	use mpyw\Co\CURLException;
+	use mpyw\Co\Co; //名前空間の設定
 	use mpyw\Cowitter\Client;
-//	use mpyw\Cowitter\HttpException;
 
 	define('MYNAME','@onoonokatio'); //ツイートの中から消すために、自分のツイッターIDを定義
 
-	try {
-		Co::wait(function () {
-
-
 			$client = new Client([CK, CS, AT, ATS]); //API情報からクライアントオブジェクトを作成
-			/*yield $client->streamingAsync('user', function ($status) { //ツイートを取得した時の動作
+			$tasks[] = $client->streamingAsync('user',function($status){ //ストリームにツイートが流れてくるたびに呼ばれる
+				//var_dump($status);
 				if(strpos($status->text,MYNAME)===TRUE){
 					$get = str_replace(array(MYNAME,"\n"),array('',' '),$status->text);
 					if(strpos($get,' tip')!==false){
@@ -27,13 +22,15 @@
 						//SELECT adddres FROM tipxem.account WHERE uid == $uid;
 					}
 				}
-			});*/
-//			yield $task;
-		var_dump($status);
+			});
 
-
+	try {
+		Co::wait(function () {
+			global $tasks;
+			yield $tasks;
 		});
 	} catch (RuntimeException $e) { //エラーが起こった時の情報を変数$eに入れる
-		exit("Error: {$e->getMessage()}\n"); //エラーが起きたらメッセージを表示して終了
+		exit("Error: {$e->getMessage()}"); //エラーが起きたらメッセージを表示して終了
+		//todo これを例外にする
 	}
 ?>
