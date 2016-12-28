5cd1bf74a03930b9af9a6fd7ab9d2bd3aa18f739
onokatio <onokatio@gmail.com>
2016-12-28 19:47:39 +0900

    change name

--- main.php
+++ main.php
@@ -9,7 +9,6 @@
 
 	Co::wait(function () {
 
-
 			$client = new Client([CK, CS, AT, ATS]); //API情報からクライアントオブジェクトを作成
 			yield $client->streamingAsync('user',function($status) use ($client){ //ストリームにツイートが流れてくるたびに呼ばれる
 				var_dump($status);
@@ -19,12 +18,9 @@
 					if(strpos($get,' tip ')!==false){ //tipの文字が含まれていたら
 						$get = str_replace(' tip ','',$get); //tipを消す
 						$get = explode(' ',$get,3); //配列に押し込む
-						if(ctype_digit($get[1]) && $get[0][0] == '@'){ //ユーザー名に@入ってて、さらに量が文字ならば
-							//ツイートする
+						if(ctype_digit($get[1]) && $get[0][0] == '@'){ //ユーザー名に@入ってて、さらに量が数値ならば
 							yield Co::SAFE => $client->postAsync('statuses/update',['status' => "@{$status->user->screen_name} さんの{$get[1]}XEMを{$get[0]}さんにばびゅん！" ,'in_reply_to_status_id' => $status->id]);
-							yield Co::SAFE => $client->postAsync('favorites/create',['id' => $status->id]);
 						}else{
-							//ふえぇ
 							yield Co::SAFE => $client->postAsync('statuses/update',['status' => "ふえぇ…なにゆってるかわからないよぉ…" ,'in_reply_to_status_id' => $status->id]);						
 						}
 
@@ -33,20 +29,21 @@
 						$get = explode(' ',$get,3);
 						if(ctype_digit($get[1])){
 							yield Co::SAFE => $client->postAsync('statuses/update',['status' => "@{$status->user->screen_name} さんの{$get[1]}XEMを{$get[0]}にぶん投げたよ！" ,'in_reply_to_status_id' => $status->id]);
-							yield Co::SAFE => $client->postAsync('favorites/create',['id' => $status->id]);
 						}else{
 							yield Co::SAFE => $client->postAsync('statuses/update',['status' => "ふえぇ…なにゆってるかわからないよぉ…" ,'in_reply_to_status_id' => $status->id]);						
 						}
 
 					}else if(strpos($get,' balance')!==false){
 						yield Co::SAFE => $client->postAsync('statuses/update',['status' => "@{$status->user->screen_name} さんの残高は0XEMですよっ！" ,'in_reply_to_status_id' => $status->id]);
-						yield Co::SAFE => $client->postAsync('favorites/create',['id' => $status->id]);
 						//SELECT balance FROM tipxem.account WHERE uid == $uid;
 
 					}else if(strpos($get,' deposit')!==false){
 						yield Co::SAFE => $client->postAsync('statuses/update',['status' => "@{$status->user->screen_name} さんのアドレスは…残念ながらまだ作ってないです！開発者あくしろよっw！！" ,'in_reply_to_status_id' => $status->id]);
 						//SELECT adddres FROM tipxem.account WHERE uid == $uid;
+					}else{
+					
 					}
+					yield Co::SAFE => $client->postAsync('favorites/create',['id' => $status->id]);
 				}
 			});
 		});
